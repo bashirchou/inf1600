ec174c76da6717418a802758efaca5cb035d53a0
Raphael St-Jean <raphael.st-jean@polymtl.ca>
2020-03-14 16:56:43 -0400

    Ajout diagonal.s

--- inf1600_tp3/diagonal.s
+++ inf1600_tp3/diagonal.s
@@ -1,11 +1,64 @@
 .global matrix_diagonal_asm
 
 matrix_diagonal_asm:
-        push %ebp      			/* Save old base pointer */
-        mov %esp, %ebp 			/* Set ebp to current esp */
+        push %ebp      /* Save old base pointer */
+        mov %esp, %ebp /* Set ebp to current esp */
 
-		/* Write your solution here */
-		
-        leave          			/* Restore ebp and esp */
-        ret            			/* Return to the caller */
+        /* Write your solution here
+        8(%ebp) = param1
+        12(%ebp) = param2
+        16(%ebp) = param3
+        */
+        sub $8, %esp /* Fait de L'espace pour r et c */
+        movl $0, -4(%ebp)    /* Met la valeur de r dans memoire  (ligne) */
+        movl $0, -8(%ebp)    /* Met la valeur de c dans memoire (colonne)*/
+        jmp conditionBoucleLigne
+
+
+conditionBoucleLigne:
+        movl 16(%ebp), %edx  /* Met la valeur de matorder dans %edx */
+        movl -4(%ebp), %eax     /* Met la valeur de r dans %eax  (ligne) */
+        cmp %edx, %eax
+        jl conditionBoucleColonne    /* r < matorder */
+        jmp fin
+conditionBoucleColonne:
+        movl 16(%ebp), %edx  /* Met la valeur de matorder dans %edx */
+        movl -8(%ebp), %ecx     /* Met la valeur de c dans %ecx  (COlonne) */
+        cmp %edx, %ecx
+        jl boucleColonne    /* c < matorder */
+        movl $0, -8(%ebp)    /* Remet la valeur de c a 0 (colonne) */
+        add $1, -4(%ebp)      /* ++r (colonne) */
+        jmp conditionBoucleLigne
+
+boucleColonne:
+        movl 16(%ebp), %eax  /* Met la valeur de matorder dans %eax */
+        movl -4(%ebp), %ecx     /* Met la valeur de r dans %ecx  (ligne) */
+        movl $0, %edx   /* Précaution comme montrer par le prof */
+        mul %ecx       /* edx#eax <- eax * %ecx */
+        add -8(%ebp), %eax      /* %eax <- (r * matorder) + c */
+
+        movl -4(%ebp), %ecx     /* Met la valeur de r dans %ecx  (ligne) */
+        cmp %ecx, -8(%ebp)    /* r == c */
+        je pointDiagonal
+
+        movl 12(%ebp), %ecx  /* Met la valeur de debut de outmatdata*/
+        movl  $0, (%ecx, %eax,4)
+
+        add $1, -8(%ebp)    /* ++c  */
+        jmp conditionBoucleColonne
+
+pointDiagonal:
+        movl 8(%ebp), %edx  /* Met la valeur de debut de inmatdata*/
+        movl 12(%ebp), %ecx  /* Met la valeur de debut de outmatdata*/
+
+        movl (%edx,%eax,4), %edx  /* Enregistre la valeur de inmatdata a la bonne position dans %edx */
+        movl  %edx, (%ecx, %eax,4)  /* outmatdata[c + r * matorder] = inmatdata[c + r * matorder];  */
+
+        add $1, -8(%ebp)    /* ++c  */
+        jmp conditionBoucleColonne
+
+fin:
+        mov %ebp, %esp  /* Enleve espace de r,c */
+        leave          /* Restore ebp and esp */
+        ret            /* Return to the caller */
 
